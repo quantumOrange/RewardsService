3911ba682fe87efed4558631897cef032e13a20d
David Crooks <crodave@gmail.com>
2019-03-24 16:19:37 +0100

    enums for channels, rewards and eligibility

---
+++ SkyRewards/enums.hpp
@@ -0,0 +1,33 @@
+//
+//  enums.h
+//  Sky
+//
+//  Created by David Crooks on 23/03/2019.
+//  Copyright © 2019 David Crooks. All rights reserved.
+//
+
+#ifndef enums_h
+#define enums_h
+
+enum Reward {
+    CHAMPIONS_LEAGUE_FINAL_TICKET,
+    KARAOKE_PRO_MICROPHONE,
+    PIRATES_OF_THE_CARIBBEAN_COLLECTION
+};
+
+enum Channel {
+    SPORTS,
+    KIDS,
+    MUSIC,
+    NEWS,
+    MOVIES
+};
+
+enum CustomerEligible {
+    ELIGABLE,
+    INELIGABLE,
+    INVALID_ACCOUNT_ID,
+    SERVICE_FAILIURE
+};
+
+#endif /* enums_h */
