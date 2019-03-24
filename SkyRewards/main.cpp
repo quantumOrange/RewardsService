a0ec97845d2cd04323139698add020c23a4799a5
David Crooks <crodave@gmail.com>
2019-03-24 16:09:39 +0100

    Initial Commit

---
+++ SkyRewards/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SkyRewards
+//
+//  Created by David Crooks on 24/03/2019.
+//  Copyright © 2019 David Crooks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
